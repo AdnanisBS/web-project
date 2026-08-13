0d22bb45d96275b80c42fa345b7511bbf81c088d
AdnanisBS <adnanmi2007@gmail.com>
2026-08-13 17:05:04 +0530

    finished 1st session jee mains

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,27 @@
-alert("yes it worky")
+// alert("yes it worky")
+const users = [
+  {
+    name: "John Doe",
+    gender: "Male",
+    image: "john.png",
+  },
+  {
+    name: "Jane Doe",
+    gender: "Female",
+    image: "jane.png",
+  },
+];
+
+var curIndex = 0;
+
+function toggle() {
+  if (curIndex == 0) {
+    curIndex = 1;
+  } else {
+    curIndex = 0;
+  }
+
+  document.getElementById("card-image").src = users[curIndex].image;
+  document.getElementById("card-gender").innerText = users[curIndex].gender;
+  document.getElementById("card-name").innerText = users[curIndex].name;
+}
