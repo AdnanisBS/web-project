fedf15653f4eaaa9c99bac73a5e34ff03f33102a
AdnanisBS <adnanmi2007@gmail.com>
2026-08-13 15:09:22 +0530

    html

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 8080
-app.use(express.static("front"))
+app.use(express.static('frontend'))
 app.listen(port,function(){
     console.log("yes it worky :) at http://localhost:"+port)
 })
